da5e8fa43049aca066259b4818706b3fbd370a5b
Ира <irinashinkarenko03@gmail.com>
2024-04-29 23:26:29 +0500

    first commit

---
+++ first_task.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func how_many_computers(number int) string {
+	if number >= 5 && number <= 20 {
+		return fmt.Sprintf("%d компьютеров", number)
+	}
+	remainder := number % 10
+	switch remainder {
+	case 1:
+		return fmt.Sprintf("%d компьютер", number)
+	case 2, 3, 4:
+		return fmt.Sprintf("%d компьютера", number)
+	default:
+		return fmt.Sprintf("%d компьютеров", number)
+	}
+}
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	fmt.Println(how_many_computers(n))
+}
